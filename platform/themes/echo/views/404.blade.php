27852fae8be19ca9b1bc2882a2288123eccdc8c9
arseldev <michaelarselius@gmail.com>
2025-02-22 11:38:25 +0800

    modified text error 404

--- platform/themes/echo/views/404.blade.php
+++ platform/themes/echo/views/404.blade.php
@@ -14,13 +14,16 @@
                             <h1>404</h1>
                         </div>
                         <div class="echo-error-sub-heading">
-                            <h3>{{ __('Page not found') }}</h3>
+                            <h3>{{ __('Halaman tidak ditemukan') }}</h3>
                         </div>
                         <div class="echo-error-pera">
-                            <p>{!! BaseHelper::clean(__('Sorry, the page you seems looking for,<br> has been moved, redirected or moved permanently.')) !!}</p>
+                            <p>{!! BaseHelper::clean(
+                                __('Maaf, halaman yang Anda cari,<br> telah dipindahkan, dialihkan, atau dipindahkan secara permanen.'),
+                            ) !!}</p>
                         </div>
                         <div class="error-btn">
-                            <a href="{{ BaseHelper::getHomepageUrl() }}" class="text-capitalize">{{ __('Go back home') }}</a>
+                            <a href="{{ BaseHelper::getHomepageUrl() }}"
+                                class="text-capitalize">{{ __('Kembali ke Beranda') }}</a>
                         </div>
                     </div>
                 </div>
@@ -28,5 +31,3 @@
         </section>
     </div>
 @endsection
-
-
